2365709217911dfd3a76a743379410b0d22ad33c
renatogutierrez-web <renato.gutierrez@alumnos.ucn.cl>
2025-10-30 16:53:58 -0300

    Creacion Tablas

--- PaginaKinesiologia/applications/curso/models.py
+++ PaginaKinesiologia/applications/curso/models.py
@@ -3,32 +3,31 @@ from django.db import models
 # Create your models here.
 class Curso(models.Model):
     nombre = models.CharField('Nombre del Curso', max_length=150, null=False)
-    descripcion = models.CharFieldField('Descripción', max_length = 500, null=False)
-    programa = models.TextFields('Programa Asignatura', max_length=150, null=False)
-    estado = models.BooleanField('Estado del Curso', null =False)
-    docente= models.ForeignKey(Docente, on_delete=models.CASCADE)
-    objetivo = models.ForeignKey(Aprendizajeesperado, on_delete=models.CASCADE)
-
+    descripcion = models.TextField('Descripción', null=False)
+    programa = models.TextField('Programa de asignatura', null=False)
+    estado = models.BooleanField('Estado del curso', null=False)
+    docente = models.ForeignKey('Docentes', on_delete=models.CASCADE)
+    objetivo = models.ForeignKey('Aprendizajeesperado', on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.id) + '-' + self.nombre
-        
 
-        
+
+
 class Progreso(models.Model):
-    nombre = models.CharField('Progreso Estudiante', max_length=150, null=False)
-    descripcion = models.CharField('Avance Estudiante',max_length= 150, null=False)
-    curso = models.ForeignKey(Curso, null=False)
-    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    nombre = models.CharField('Progreso del Estudiante', max_length=150, null=False)
+    descripcion = models.TextField('Avance', null=False)
+    curso = models.ForeignKey('Cursos', on_delete=models.CASCADE)
+    estudiante = models.ForeignKey('Estudiante', on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + '-' + self.nombre
-        
-class Solicitud_Estudiante(models.Model):
-    nombre = models.CharField('Solicitud de Inscripcion del Curso', max_length=150, null=False)
-    estado = models.BooleanField('Estado de la Solicitud', null =False)
-    curso = models.ForeignKey(Curso, null=False)
-    estudiante = models.ForeignKey(Estudiante, null =False)
-    docente= models.ForeignKey(Docente, on_delete=models.CASCADE)
 
+class Solicitud_inscripcion(models.Model):
+    nombre = models.CharField('Solicitud de Inscripcion', max_length=150, null=False)
+    estado = models.BooleanField('Estado de Inscripcion', null=False)    
+    curso = models.ForeignKey('Cursos', on_delete=models.CASCADE)
+    estudiante = models.ForeignKey('Estudiante', on_delete=models.CASCADE)
+    docente = models.ForeignKey('Docentes', on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + '-' + self.nombre
