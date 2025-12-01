2e06d763915024c3b634acfdf78d34f7b7a7778a
marianaferreira-ucn <mariana.ferreira@alumnos.ucn.cl>
2025-12-01 15:20:37 -0300

    Sprint 4

--- PaginaKinesiologia/applications/Caso_Clínico/apps.py
+++ PaginaKinesiologia/applications/Caso_Clínico/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+<<<<<<<< Updated upstream:PaginaKinesiologia/applications/Caso_Clínico/apps.py
 
 class CasoClínicoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications.Caso_Clínico'
+========
+class InicioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applications.aprendizajeesperado'
+>>>>>>>> Stashed changes:PaginaKinesiologia/applications/aprendizajeesperado/apps.py
--- PaginaKinesiologia/applications/Tema/apps.py
+++ PaginaKinesiologia/applications/Tema/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+<<<<<<<< Updated upstream:PaginaKinesiologia/applications/Tema/apps.py
 
 class TemaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications.Tema'
+========
+class InicioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applications.login'
+>>>>>>>> Stashed changes:PaginaKinesiologia/applications/login/apps.py
